7d92597a920ea75da7520cb895889c2d436bb567
Kevin Black <kpbinc@hinge.(none)>
2013-01-23 08:31:35 -0700

    updated tomcat setup script to set JAVA_HOME

--- launch/setup_tomcat.sh
+++ launch/setup_tomcat.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # tomcat needs a jdk
 sudo apt-get -y install openjdk-6-jdk
+export JAVA_HOME=/usr/lib/jvm/java-6-openjdk
 sudo apt-get -y install tomcat7
 sudo cp ./launch/tomcat7-default.txt /etc/default/tomcat7
 sudo cp ./launch/tomcat7-server.xml /etc/tomcat7/server.xml
-sudo sh -c 'echo "Kevin Black on tomcat 7" > /var/lib/tomcat7/webapps/ROOT/index.html'
+sudo sh -c 'echo "Kevin Black on Tomcat 7" > /var/lib/tomcat7/webapps/ROOT/index.html'
 sudo service tomcat7 start
