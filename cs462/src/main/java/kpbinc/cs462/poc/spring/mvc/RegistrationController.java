c48c5a27202987b2d8a103076997c005792ab822
Kevin Black <kevinblack100@gmail.com>
2013-01-30 09:08:45 -0800

    lab 1: added RegistrationController and view, currently not using EL.

---
+++ cs462/src/main/java/kpbinc/cs462/poc/spring/mvc/RegistrationController.java
@@ -0,0 +1,20 @@
+package kpbinc.cs462.poc.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/registration")
+public class RegistrationController {
+
+	@RequestMapping(value = "/view/{event-id}")
+	public String view(
+			@PathVariable("event-id") Long eventID,
+			ModelMap model) {
+		model.addAttribute("eventID", eventID);
+		return "registration/view";
+	}
+	
+}
