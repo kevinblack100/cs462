8986228f3592de7c3768cec73432b0ad9b8f97f6
Kevin Black <kevinblack100@gmail.com>
2013-01-29 19:12:37 -0800

    lab 1: replaced the mvc:view-controller mapping in cs462-servlet.xml with an annotated HelloController

---
+++ cs462/src/main/java/kpbinc/cs462/poc/spring/mvc/HelloController.java
@@ -0,0 +1,16 @@
+package kpbinc.cs462.poc.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+
+	@RequestMapping
+	public String hello() {
+		// return the name of the view to render
+		return "hello";
+	}
+	
+}
