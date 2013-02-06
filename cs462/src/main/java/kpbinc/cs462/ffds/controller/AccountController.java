fede9819d095540d13c44bf822ca8171cd15763f
Kevin Black <kevinblack100@gmail.com>
2013-02-06 11:57:52 -0800

    lab 1: AccountController now adds an account record to the users.properties file on account registration. This works on the deployed webapp but not testing it in Eclipse.

--- cs462/src/main/java/kpbinc/cs462/ffds/controller/AccountController.java
+++ cs462/src/main/java/kpbinc/cs462/ffds/controller/AccountController.java
@@ -1,9 +1,13 @@
 package kpbinc.cs462.ffds.controller;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -28,6 +32,9 @@ public class AccountController {
 	
 	@Autowired
 	private InMemoryUserDetailsManager userDetailsManager;
+	
+	@Autowired
+	private ServletContext servletContext;
 	
 	public AccountController() {
 		int tmpbrkpnt = 1;
@@ -65,6 +72,20 @@ public class AccountController {
 			
 			UserDetails newRegistrantDetails = new User(username, getDefaultPassword(), authorities);
 			userDetailsManager.createUser(newRegistrantDetails);
+			
+			// write entry to the users.properties file as well
+			try {
+				String filePath = servletContext.getRealPath("/WEB-INF/ffds/users.properties");
+				File userPropertiesFile = new File(filePath);
+				boolean append = true;
+				FileWriter writer = new FileWriter(userPropertiesFile, append);
+				String accountEntry = String.format("%s=%s,%s,enabled", username, getDefaultPassword(), "ROLE_USER");
+				writer.write(accountEntry);
+				writer.close();
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
 			
 			redirectLocation = response.encodeRedirectURL("/cs462/ffds/secure/signin/query");
 		}
