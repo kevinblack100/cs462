1912b9c33aeca77149f7169e2fbad1a82735d967
Kevin Black <kevinblack100@gmail.com>
2013-02-06 09:33:21 -0800

    lab 1: set up ssl support

--- launch/config_tomcat.sh
+++ launch/config_tomcat.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo cp ./launch/tomcat7-default.txt /etc/default/tomcat7
 sudo cp ./launch/tomcat7-server.xml /etc/tomcat7/server.xml
+sudo cp ./launch/tomcat7-keystore.keystore /etc/tomcat7/.keystore
 sudo sh -c 'echo "Kevin Black on Tomcat 7" > /var/lib/tomcat7/webapps/ROOT/index.html'
