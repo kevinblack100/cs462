87ec9d42e82e412ffcda3fed00498c5f70684df4
Kevin Black <kevinblack100@gmail.com>
2013-02-05 21:39:00 -0800

    lab 1: split out tomcat configuration script from tomcat setup script. Edited the template server.xml to enable the SSL port. Will need to fix tomcat7 installation before testing.

--- launch/setup_tomcat.sh
+++ launch/setup_tomcat.sh
@@ -3,7 +3,5 @@
 sudo apt-get -y install openjdk-6-jdk
 export JAVA_HOME=/usr/lib/jvm/java-6-openjdk
 sudo apt-get -y install tomcat7
-sudo cp ./launch/tomcat7-default.txt /etc/default/tomcat7
-sudo cp ./launch/tomcat7-server.xml /etc/tomcat7/server.xml
-sudo sh -c 'echo "Kevin Black on Tomcat 7" > /var/lib/tomcat7/webapps/ROOT/index.html'
+./luanch/config_tomcat.sh
 sudo service tomcat7 start
