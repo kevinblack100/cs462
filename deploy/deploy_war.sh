a2fde1053eb3124f2b6a8931fd0f1b9671873da7
Kevin Black <kevinblack100@gmail.com>
2013-02-06 13:19:04 -0800

    deployment: added deployment scripts

---
+++ deploy/deploy_war.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "TODO deploy the war"
---
+++ deploy/refresh_repo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "> cd ~/repos/cs462/"
+cd ~/repos/cs462/
+
+echo "> git checkout deploy"
+git checkout deploy
+
+echo "> git pull origin deploy"
+git pull origin deploy
+
+echo "> ./deploy/deploy_war.sh"
+./deploy/deploy_war.sh
+
+echo "> git checkout master"
+git checkout master
