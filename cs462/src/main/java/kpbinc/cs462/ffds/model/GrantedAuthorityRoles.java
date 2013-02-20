43fa89b4c9c4dfcc9d37eb24aeefa337bd83801b
Kevin Black <kevinblack100@gmail.com>
2013-02-20 13:33:00 -0700

    lab 2: added ROLE_ADMIN to GrantedAuthorityRoles

--- cs462/src/main/java/kpbinc/cs462/ffds/model/GrantedAuthorityRoles.java
+++ cs462/src/main/java/kpbinc/cs462/ffds/model/GrantedAuthorityRoles.java
@@ -8,6 +8,9 @@ public class GrantedAuthorityRoles {
 	private static final String ROLE_USER_NAME = "ROLE_USER";
 	public static final GrantedAuthority ROLE_USER = new SimpleGrantedAuthority(ROLE_USER_NAME);
 	
+	private static final String ROLE_ADMIN_NAME = "ROLE_ADMIN";
+	public static final GrantedAuthority ROLE_ADMIN = new SimpleGrantedAuthority(ROLE_ADMIN_NAME);
+	
 	private static final String ROLE_DRIVER_NAME = "ROLE_DRIVER";
 	public static final GrantedAuthority ROLE_DRIVER = new SimpleGrantedAuthority(ROLE_DRIVER_NAME);
 	
