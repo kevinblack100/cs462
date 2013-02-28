37a135021c44075b6d68625e27dda4e15b9e1c5c
Kevin Black <kevinblack100@gmail.com>
2013-02-27 21:01:38 -0700

    lab 3: shop: removed now not need OAuth stuff from UsersController

--- shop/src/main/java/kpbinc/cs462/shop/controller/UsersController.java
+++ shop/src/main/java/kpbinc/cs462/shop/controller/UsersController.java
@@ -3,16 +3,12 @@ package kpbinc.cs462.shop.controller;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.logging.Logger;
 
 import javax.servlet.ServletContext;
 
-import kpbinc.cs462.shared.model.manage.AuthorizationTokenManager;
 import kpbinc.cs462.shared.model.manage.InMemoryPersistentUserDetailsManager;
 import kpbinc.util.logging.GlobalLogUtils;
 
-import org.scribe.exceptions.OAuthConnectionException;
-import org.scribe.model.Token;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
@@ -25,24 +21,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/users")
 public class UsersController extends ShopBaseSiteContextController {
 	
-	//= Class Data =====================================================================================================
-	
-	private static final Logger logger = Logger.getLogger(UsersController.class.getName());
-	
-	
 	//= Member Data ====================================================================================================
 
-	@Autowired
-	private OAuthController oauthController;
-	
 	@Autowired
 	private ServletContext servletContext;
 	
 	@Autowired
 	private InMemoryPersistentUserDetailsManager userDetailsManager;
-	
-	@Autowired
-	private AuthorizationTokenManager authorizationTokenManager;
 	
 	
 	//= Initialization =================================================================================================
@@ -61,37 +46,12 @@ public class UsersController extends ShopBaseSiteContextController {
 		return "users/list";
 	}
 	
-	@RequestMapping("/{user-name}")
+	@RequestMapping("/{username}")
 	public String viewProfile(
 			ModelMap model,
-			@PathVariable("user-name") String username) {
+			@PathVariable("username") String username) {
 		
 		model.addAttribute("username", username);
-		
-		boolean hasFoursquareAuthToken = authorizationTokenManager.hasAuthorizationToken(username, "foursquare");
-		model.addAttribute("hasFoursquareAuthToken", hasFoursquareAuthToken);
-		
-		boolean userLoggedIn = getLoggedInUserContext().isUserLoggedIn(username);
-		model.addAttribute("userLoggedIn", userLoggedIn);
-		
-		if (hasFoursquareAuthToken) {
-			String retrieveCheckinsURL = "https://api.foursquare.com/v2/users/self/checkins?oauth_token=";
-			Token accessToken = authorizationTokenManager.getAuthorizationToken(username, "foursquare");
-			retrieveCheckinsURL += accessToken.getToken();
-			retrieveCheckinsURL += "&sort=newestfirst";
-			if (!userLoggedIn) {
-				retrieveCheckinsURL += "&limit=1";
-			}
-			
-			try {
-				String checkinJsonData = oauthController.getDetailsForUser("foursquare", retrieveCheckinsURL, username);
-				model.addAttribute("checkins", checkinJsonData);
-			}
-			catch (OAuthConnectionException e) {
-				logger.info("Caught exception: " + e.getMessage());
-				e.printStackTrace();
-			}
-		}
 		
 		return "users/profile";
 	}
