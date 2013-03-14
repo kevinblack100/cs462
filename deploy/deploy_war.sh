5958c725454b3a6c32b5cd5762cd8a5dadff92ba
Kevin Black <kevinblack100@gmail.com>
2013-03-13 18:07:19 -0600

    deploy: added driver and shop war files for lab 3, updated deploy_war.sh to remove all old webapps and to install the new ones

--- deploy/deploy_war.sh
+++ deploy/deploy_war.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
+# Stop Tomcat
 echo "> sudo service tomcat7 stop"
 sudo service tomcat7 stop
 
+# Remove old Webapps
 echo "> sudo rm -rf /var/lib/tomcat7/webapps/cs462/"
 sudo rm -rf /var/lib/tomcat7/webapps/cs462/
 
-echo "> sudo cp ./deploy/cs462.war /var/lib/tomcat7/webapps/"
-sudo cp ./deploy/cs462.war /var/lib/tomcat7/webapps/
+echo "> sudo rm -rf /var/lib/tomcat7/webapps/shop/"
+sudo rm -rf /var/lib/tomcat7/webapps/shop/
 
+echo "> sudo rm -rf /var/lib/tomcat7/webapps/driver/"
+sudo rm -rf /var/lib/tomcat7/webapps/driver/
+
+# Install new Webapps
+echo "> sudo cp ./deploy/shop.war /var/lib/tomcat7/webapps/"
+sudo cp ./deploy/shop.war /var/lib/tomcat7/webapps/
+
+echo "> sudo cp ./deploy/driver.war /var/lib/tomcat7/webapps/"
+sudo cp ./deploy/driver.war /var/lib/tomcat7/webapps/
+
+# Restart Tomcat
 echo "> sudo service tomcat7 start"
 sudo service tomcat7 start
