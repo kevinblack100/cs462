cee80ff6bba6fd3320a035058326306d8ac4915a
Kevin Black <kevinblack100@gmail.com>
2013-03-22 14:41:49 -0600

    lab 4: guild: added flower shop registration and list page to the guild along with necessary controller (FlowerShopProfilesController), and config in guild-spring-context-servlet.xml, and view definition in shops_list.jsp and style in general/tables.css.

---
+++ guild/src/main/java/kpbinc/cs462/guild/controller/FlowerShopProfilesController.java
@@ -0,0 +1,79 @@
+package kpbinc.cs462.guild.controller;
+
+import java.util.Collection;
+import java.util.logging.Logger;
+
+import kpbinc.cs462.shared.model.FlowerShopProfile;
+import kpbinc.cs462.shared.model.manage.FlowerShopProfileManager;
+import kpbinc.util.logging.GlobalLogUtils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@Scope(value = "request")
+@RequestMapping(value = "/shops")
+public class FlowerShopProfilesController extends GuildBaseSiteContextController {
+
+	//= Class Data =====================================================================================================
+	
+	private static final Logger logger = Logger.getLogger(FlowerShopProfilesController.class.getName());
+	
+	
+	//= Member Data ====================================================================================================
+
+	@Autowired
+	private FlowerShopProfileManager flowerShopProfileManager;
+	
+	
+	//= Initialization =================================================================================================
+	
+	public FlowerShopProfilesController() {
+		GlobalLogUtils.logConstruction(this);
+	}
+
+	
+	//= Interface ======================================================================================================
+	
+	//- Web API --------------------------------------------------------------------------------------------------------
+	
+	@RequestMapping
+	public String getShopsList(ModelMap model) {
+		Collection<FlowerShopProfile> flowerShopProfiles = flowerShopProfileManager.retrieveAll();
+		model.addAttribute("flowerShopProfiles", flowerShopProfiles);
+		return "shops/shops_list";
+	}
+	
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
+	public String createShop(
+			@RequestParam(value = "name", required = true) String name,
+			@RequestParam(value = "location", required = true) String location,
+			@RequestParam(value = "latitude", required = true) Double latitude,
+			@RequestParam(value = "longitude", required = true) Double longitude) {
+		
+		if (   name != null
+			&& location != null
+			&& latitude != null
+			&& longitude != null) {	
+			
+			FlowerShopProfile profile = new FlowerShopProfile();
+			profile.setName(name);
+			profile.setLocation(location);
+			profile.setLatitude(latitude);
+			profile.setLongitude(longitude);
+			
+			flowerShopProfileManager.register(profile);
+		}
+		else {
+			// TODO set error message
+		}
+		
+		return "redirect:/" + getContextPaths().getDynamicRelativePath() + "/shops/";
+	}
+	
+}
