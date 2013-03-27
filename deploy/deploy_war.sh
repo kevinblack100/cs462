971c361d38bd18dcbd982795532238ed2ba079e1
Kevin Black <kevinblack100@gmail.com>
2013-03-27 15:27:44 -0600

    deploy: added updated wars for shop, driver, and guild and updated deploy script. Will need to get another driver.war version since it contains old files.

--- deploy/deploy_war.sh
+++ deploy/deploy_war.sh
@@ -4,21 +4,27 @@ echo "> sudo service tomcat7 stop"
 sudo service tomcat7 stop
 
 # Remove old Webapps
-echo "> sudo rm -rf /var/lib/tomcat7/webapps/cs462/"
-sudo rm -rf /var/lib/tomcat7/webapps/cs462/
+echo "> sudo rm -rf /var/lib/tomcat7/webapps/cs462"
+sudo rm -rf /var/lib/tomcat7/webapps/cs462
 
-echo "> sudo rm -rf /var/lib/tomcat7/webapps/shop/"
-sudo rm -rf /var/lib/tomcat7/webapps/shop/
+echo "> sudo rm -rf /var/lib/tomcat7/webapps/shop"
+sudo rm -rf /var/lib/tomcat7/webapps/shop
 
-echo "> sudo rm -rf /var/lib/tomcat7/webapps/driver/"
-sudo rm -rf /var/lib/tomcat7/webapps/driver/
+echo "> sudo rm -rf /var/lib/tomcat7/webapps/driver"
+sudo rm -rf /var/lib/tomcat7/webapps/driver
+
+echo "> sudo rm -rf /var/lib/tomcat7/webapps/guild"
+sudo rm -rf /var/lib/tomcat7/webapps/guild
 
 # Install new Webapps
-echo "> sudo cp ./deploy/shop.war /var/lib/tomcat7/webapps/"
-sudo cp ./deploy/shop.war /var/lib/tomcat7/webapps/
+echo "> sudo cp ./deploy/shop.war /var/lib/tomcat7/webapps"
+sudo cp ./deploy/shop.war /var/lib/tomcat7/webapps
 
-echo "> sudo cp ./deploy/driver.war /var/lib/tomcat7/webapps/"
-sudo cp ./deploy/driver.war /var/lib/tomcat7/webapps/
+echo "> sudo cp ./deploy/driver.war /var/lib/tomcat7/webapps"
+sudo cp ./deploy/driver.war /var/lib/tomcat7/webapps
+
+echo "> sudo cp ./deploy/guild.war /var/lib/tomcat7/webapps"
+sudo cp ./deploy/guild.war /var/lib/tomcat7/webapps
 
 # Restart Tomcat
 echo "> sudo service tomcat7 start"
