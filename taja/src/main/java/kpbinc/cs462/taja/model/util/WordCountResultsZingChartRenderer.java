6c713467e615574f1691aca193be12243917b383
Kevin Black <kevinblack100@gmail.com>
2013-04-08 17:00:17 -0600

    lab 5: taja: WordCountResultsZingChartRenderer: added getQueryString with a words list parameter so that a smaller chart can be created with a sample of the words.

--- taja/src/main/java/kpbinc/cs462/taja/model/util/WordCountResultsZingChartRenderer.java
+++ taja/src/main/java/kpbinc/cs462/taja/model/util/WordCountResultsZingChartRenderer.java
@@ -1,9 +1,77 @@
 package kpbinc.cs462.taja.model.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.Validate;
+
 import kpbinc.cs462.taja.model.WordCountResults;
 import kpbinc.net.UTF8URLEncoder;
 import kpbinc.util.logging.GlobalLogUtils;
 
+/* Example bar chart code from ZingChart
+{
+	"graphset": [
+		{
+			"type":"bar",
+			"background-color":"#ffffff",
+			"scale-x": {
+				"line-color":"#ffffff",
+				"values": ["Jan","Feb","Mar","Apr","May","Jun"],
+				"guide": {
+					"visible":false
+				},
+				"tick": {
+					"visible":false
+				},
+				"item": {
+					"font-family":"helvetica",
+					"font-color":"#05636c",
+					"font-weight":"bold",
+					"font-size":"12px",
+					"visible":true
+				}
+			},
+			"scale-y": {
+				"values":"0:50:10",
+				"line-width":"0px",
+				"format":"%v",
+				"tick": {
+					"visible":false
+				},
+				"item": {
+					"font-family":"helvetica",
+					"font-color":"#05636c",
+					"font-weight":"bold",
+					"font-size":"12px",
+					"visible":true
+				},
+				"guide": {
+					"visible":false
+				}
+			},
+			"tooltip": {
+				"visible":true
+			},
+			"plot": {
+				"tooltip-text":" %v  %t <br> Key: %k"
+			},
+			"series": [
+				{
+					"values":["28","35","19","7","30","46","38"],
+					"text":"Shoes",
+					"background-color":"#05636c"
+				},
+				{
+					"values":["40","35","26","23","27","22","30"],
+					"text":"Pants",
+					"background-color":"#69a1a7"
+				}
+			]
+		}
+	]
+}
+*/
 public class WordCountResultsZingChartRenderer {
 
 	//= Initialization =================================================================================================
@@ -16,10 +84,23 @@ public class WordCountResultsZingChartRenderer {
 	//= Interface ======================================================================================================
 	
 	public String getQueryString(WordCountResults results) {
+		Validate.notNull(results, "results must not be null");
+		Validate.notNull(results.getWordCounts(), "results.wordCounts must not be null");
+		
+		String queryString = getQueryString(results, new ArrayList<String>(results.getWordCounts().keySet()));
+		return queryString;
+	}
+	
+	public String getQueryString(WordCountResults results, List<String> words) {
+		Validate.notNull(results, "results must not be null");
+		Validate.notNull(results.getWordCounts(), "results.wordCounts must not be null");
+		Validate.notNull(words, "words must not be null");
+		Validate.isTrue(!words.isEmpty(), "words must not be empty");
+		
 		StringBuilder builder = new StringBuilder();
 		builder.append("{\"graphset\":[{\"type\":\"bar\",\"scale-x\":{\"values\":[");
 		boolean firstWord = true;
-		for (String word : results.getWordCounts().keySet()) {
+		for (String word : words) {
 			if (!firstWord) {
 				builder.append(",");
 			}
@@ -28,7 +109,9 @@ public class WordCountResultsZingChartRenderer {
 		}
 		builder.append("]},\"series\":[{\"values\":[");
 		boolean firstCount = true;
-		for (Long count : results.getWordCounts().values()) {
+		for (String word : words) {
+			Long count = results.getWordCounts().get(word);
+			count = (count != null ? count : 0L);
 			if (!firstCount) {
 				builder.append(",");
 			}
@@ -42,70 +125,6 @@ public class WordCountResultsZingChartRenderer {
 		String queryString = String.format("d=%s", dataUrlEncoding);
 		
 		return queryString;
-		
-		/* Example bar chart code from ZingChart
-		{
-			"graphset": [
-				{
-					"type":"bar",
-					"background-color":"#ffffff",
-					"scale-x": {
-						"line-color":"#ffffff",
-						"values": ["Jan","Feb","Mar","Apr","May","Jun"],
-						"guide": {
-							"visible":false
-						},
-						"tick": {
-							"visible":false
-						},
-						"item": {
-							"font-family":"helvetica",
-							"font-color":"#05636c",
-							"font-weight":"bold",
-							"font-size":"12px",
-							"visible":true
-						}
-					},
-					"scale-y": {
-						"values":"0:50:10",
-						"line-width":"0px",
-						"format":"%v",
-						"tick": {
-							"visible":false
-						},
-						"item": {
-							"font-family":"helvetica",
-							"font-color":"#05636c",
-							"font-weight":"bold",
-							"font-size":"12px",
-							"visible":true
-						},
-						"guide": {
-							"visible":false
-						}
-					},
-					"tooltip": {
-						"visible":true
-					},
-					"plot": {
-						"tooltip-text":" %v  %t <br> Key: %k"
-					},
-					"series": [
-						{
-							"values":["28","35","19","7","30","46","38"],
-							"text":"Shoes",
-							"background-color":"#05636c"
-						},
-						{
-							"values":["40","35","26","23","27","22","30"],
-							"text":"Pants",
-							"background-color":"#69a1a7"
-						}
-					]
-				}
-			]
-		}
-		*/
 	}
 	
 }
