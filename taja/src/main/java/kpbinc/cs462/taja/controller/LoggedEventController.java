7a1852310a88bb037c12fa62a1c9501d3e495dcb
Kevin Black <kevinblack100@gmail.com>
2013-04-09 17:03:45 -0600

    lab 5: taja: added comparator to put the list of LoggedEvents into most- recent-first order.

--- taja/src/main/java/kpbinc/cs462/taja/controller/LoggedEventController.java
+++ taja/src/main/java/kpbinc/cs462/taja/controller/LoggedEventController.java
@@ -1,6 +1,10 @@
 package kpbinc.cs462.taja.controller;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
@@ -17,6 +21,39 @@ import kpbinc.util.logging.GlobalLogUtils;
 @RequestMapping(value = "/events/logged")
 public class LoggedEventController extends TAJABaseSiteContextController {
 
+	//= Class Data =====================================================================================================
+	
+	private static class MostRecentFirstLoggedEventComparator implements Comparator<LoggedEvent> {
+
+		private static final int BEFORE = -1;
+		private static final int AFTER = 1;
+		private static final int EQUALS = 0;
+		
+		
+		@Override
+		public int compare(LoggedEvent lhs, LoggedEvent rhs) {
+			if (lhs == null) {
+				return AFTER;
+			}
+			// else
+			if (rhs == null) {
+				return BEFORE;
+			}
+			// else
+			if (lhs.getId() < rhs.getId()) {
+				return AFTER;
+			}
+			// else
+			if (rhs.getId() < lhs.getId()) {
+				return BEFORE;
+			}
+			// else
+			return EQUALS;
+		}
+		
+	}
+	
+	
 	//= Member Data ====================================================================================================
 	
 	@Autowired
@@ -35,7 +72,8 @@ public class LoggedEventController extends TAJABaseSiteContextController {
 	
 	@RequestMapping
 	public String getLoggedEventList(ModelMap model) {
-		Collection<LoggedEvent> loggedEvents = loggedEventManager.retrieveAll();
+		List<LoggedEvent> loggedEvents = new ArrayList<LoggedEvent>(loggedEventManager.retrieveAll());
+		Collections.sort(loggedEvents, new MostRecentFirstLoggedEventComparator());
 		model.addAttribute("loggedEvents", loggedEvents);	
 		
 		return "events/logged/logged_events_list";
